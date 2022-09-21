861d4c0081cf7d118a06b4e8ac6820fd1f088862
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-09-21 12:00:09 -0400

    --prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  production: true,
+  production: false,
   supabase: {
-    publicKey: 'XXXXXXXX',
-    url: 'https://your-url.supabase.co'
+    publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzaGRxZG1jbXpwZHZqdXl4YnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzNzUwNzAsImV4cCI6MTk3ODk1MTA3MH0.EsxmOznwalwGhnjrcmmc4xDotnWs0gXZU9DJPECg8HA',
+    url: 'https://zshdqdmcmzpdvjuyxbqy.supabase.co'
   }
 };
