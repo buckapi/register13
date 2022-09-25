23eb0dfe88ebdae50eea9789953682cf258e60b5
Buckapi <112070278+buckapi@users.noreply.github.com>
2022-09-25 12:35:43 -0400

    new credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   supabase: {
-    publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzaGRxZG1jbXpwZHZqdXl4YnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzNzUwNzAsImV4cCI6MTk3ODk1MTA3MH0.EsxmOznwalwGhnjrcmmc4xDotnWs0gXZU9DJPECg8HA',
-    url: 'https://zshdqdmcmzpdvjuyxbqy.supabase.co'
+    publicKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtb3Jsd3VmcW5zenFvcmhqanB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjQxMjMxMDIsImV4cCI6MTk3OTY5OTEwMn0.FLPyX5PNpJ4eQqFFL8XZCR24aUecYkmo_4n6w5eweVE',
+    url: 'https://rmorlwufqnszqorhjjpy.supabase.co'
   }
 };
 
